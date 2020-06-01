eebc61429ca45c91b1a0c1f5f2503eb9b000cf5f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-01 21:37:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+i = 2
+range = []
+if number < 2
+  return false
+end
+while i < number-1
+  range << number % i
+  i+=1
+end
+if range.any? do |value|
+value == 0
+end
+return false
+else
+return true
+end
+end
