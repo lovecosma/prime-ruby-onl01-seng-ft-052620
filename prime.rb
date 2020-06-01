a4733910d8d92c41994aeb2f847ee59eb1d3eb62
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-01 21:07:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+i = 1
+range = []
+while i < number-1
+  range << number % i
+  i+=1
+end
+if range.any? do |value|
+value == 0
+end
+return false
+else
+return true
+end
+end
